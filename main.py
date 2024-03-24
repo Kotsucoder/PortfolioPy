0bba7a9e4444ee742f0fd1e87a68f508c42ae031
Screenbones <whoami@kotsu.red>
2024-03-24 14:18:08 -0700

    Student Project: Add content to the Website.

--- main.py
+++ main.py
@@ -13,3 +13,9 @@ with col2:
     二十四才。アメリカ人。Amateur Programmer. WaniKani Level 2. Lion. DoorDasher.
     """
     st.info(content)
+
+introText = """
+Below you can find some of the apps I have built in Python. These were created 
+while following the Python Mega Course on Udemy.
+"""
+st.write(introText)
