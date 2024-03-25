1b9677ccde3c5dd9f5e0d7e3c90fb05a2aeedf66
Screenbones <whoami@kotsu.red>
2024-03-24 23:44:43 -0700

    Add Thumbnail Titles to the Webpage

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout='wide')
 
@@ -19,3 +20,16 @@ Below you can find some of the apps I have built in Python. These were created
 while following the Python Mega Course on Udemy.
 """
 st.write(introText)
+
+col3, col4 = st.columns(2)
+
+
+data = pandas.read_csv("data.csv", sep=";")
+with col3:
+    for index, row in data[:10].iterrows():
+        st.header(row["title"])
+
+
+with col4:
+    for index, row in data[10:].iterrows():
+        st.header(row["title"])
