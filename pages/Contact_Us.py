7e2ab9c395f6705256b26036b8ee67a18a6cd38f
Screenbones <whoami@kotsu.red>
2024-03-25 11:44:43 -0700

    Send Email Via the Web Form

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,10 +1,14 @@
 import streamlit as st
+from send_email import send_email
 
 st.header("Contact Me")
 
+st.info("Warning: This form will not work due to redacted credentials.")
 with st.form(key="email_forms"):
     user_email = st.text_input("Your email address")
-    message = st.text_area("Your message")
+    raw_message = st.text_area("Your message")
+    message = f"Subject: Message from {user_email} on Streamlit\n{raw_message}"
     button = st.form_submit_button("Submit")
     if button:
-        print("I was pressed!")
+        send_email(message)
+        st.info("Your email was sent successfully")
