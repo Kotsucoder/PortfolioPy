b08b48f1f6d646ef1ea9f6d046426a3521e0d450
Screenbones <whoami@kotsu.red>
2024-03-25 12:28:25 -0700

    Secure Passwords in Environment Variables

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -3,7 +3,7 @@ from send_email import send_email
 
 st.header("Contact Me")
 
-st.info("Warning: This form will not work due to redacted credentials.")
+
 with st.form(key="email_forms"):
     user_email = st.text_input("Your email address")
     raw_message = st.text_area("Your message")
